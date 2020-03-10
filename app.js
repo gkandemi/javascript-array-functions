c2f8399bf1a9ef2e022b81e08bbb1e334bf1ffdd
gkandemi <gokhan@gkandemir.com>
2020-03-10 17:19:03 +0300

    map ile filter örneği. reduce() some(9 every() startsWith() endsWith() flat() fonksiyonları

--- app.js
+++ app.js
@@ -1231,4 +1231,77 @@ filtered = data.map(item => {
   };
 });
 
-console.log("filtered", filtered);
+// DB istekleri... Servisin gereksinimleri..
+// name, email, phone, age, lat, long, friends
+let willSaveData = data
+  .filter(i => i.name <= 20)
+  .map(item => {
+    return {
+      name: item.name,
+      email: item.email,
+      phone: item.phone,
+      age: item.age,
+      lat: item.latitude,
+      long: item.longitude,
+      picture: item.picture,
+      friends: item.friends.map(f => f.name)
+    };
+  });
+// console.log("willSaveData", willSaveData);
+
+// .reduce()
+
+// [1,2,3,4,5,6]
+// [{}, {}, {}, {}]
+
+// toplam..
+let ageSum = data.reduce((sum, item) => sum + item.age, 0);
+// console.log("ageSum", ageSum);
+
+// .some()
+let result = data.some(item => item.gender == "female" && item.age > 25);
+
+// .every()
+// result = data.every(item => item.hasOwnProperty("isActive"));
+result = data.every(item => item.age > 10);
+// console.log("REsult", result);
+
+let name = "Gokhan Kandemir";
+// startsWith()
+console.log(name.startsWith("Gok"));
+// endsWith()
+console.log(name.endsWith("mir"));
+
+let persons = [
+  {
+    name: "Gokhan",
+    fv_1: "Field 1",
+    fv_2: "Field 2"
+  },
+  {
+    name: "Defne",
+    fv_1: "Field 11",
+    fv_2: "Field 21",
+    fv_3: "Field 31"
+  },
+  {
+    name: "Handan",
+    fv_1: "Field 12",
+    fv_3: "Field 32"
+  }
+];
+
+let final_persons = persons.map(item => {
+  Object.keys(item).forEach(key => {
+    if (!key.startsWith("fv_")) {
+      delete item[key]; // delete item.name
+    }
+  });
+  return item;
+});
+
+// console.log("final_persons", final_persons);
+
+// .flat()
+let myArray = [1, 2, 3, 4, 5, [6, 7], [8, 9, [10, 11, [12, 13, [14, 15]]]]];
+console.log(myArray.flat(Infinity));
